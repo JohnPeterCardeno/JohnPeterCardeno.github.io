97f3e66ec9e6dcc77347cc9a923edc27731dbc6f
JohnPeterCardeno <johnpetercardeno@gmail.com>
2024-06-08 11:46:33 +0800

    Update index.js

--- index.js
+++ index.js
@@ -29,12 +29,11 @@ function hitdisLikes2() {
 }
 btndisLike1.addEventListener("click",hitdisLikes1)
 btndisLike2.addEventListener("click",hitdisLikes2)
-
 const submit = document.getElementById("submit")  
 const comment = document.getElementById("comment") 
 const commentbox = document.getElementById("commentbox")  
-function submitComment(){   
+function submitComment() {   
    commentbox.textContent += comment.value.toString() + "\n" 
-   comment.value = " ";
+   comment.value = "";
 } 
 submit.addEventListener("click", submitComment)
